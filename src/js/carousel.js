4479fe063befb0d0b96a6598b207b9bda066bd52
Hira <Hira@Hiras-MacBook-Pro.local>
2022-04-03 19:16:14 +0600

    lottery page completed

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
-  var owl = $(".owl-carousel");
+  var owl = $("#owl-home");
+  owl.owlCarousel({
+    margin: 10,
+    nav: true,
+    loop: true,
+    responsive: {
+      //   0: {
+      //     items: 1,
+      //   },
+      //   600: {
+      //     items: 3,
+      //   },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+});
+$(document).ready(function () {
+  var owl = $("#owl-lottery");
   owl.owlCarousel({
     margin: 10,
     nav: true,
